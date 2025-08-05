cb395c848a5c92e9f1d924f2cdaad395583b908f
hellparr <hellenparra02@gmail.com>
2025-08-05 18:07:44 -0400

    feat: add new auth system based in username and password

--- src/main/java/co/pawcare/app/controller/IndexController.java
+++ src/main/java/co/pawcare/app/controller/IndexController.java
@@ -3,6 +3,8 @@ package co.pawcare.app.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,4 +15,20 @@ public class IndexController {
     public String showRegisterForm(Model model) {
         return "index";
     }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String usuario,
+                        @RequestParam String contrasena,
+                        Model model) {
+
+        // AUTENTICACIÓN SIMPLIFICADA
+        if ("admin".equals(usuario) && "1234".equals(contrasena)) {
+            model.addAttribute("mensaje", "Autenticación satisfactoria. ¡Bienvenido " + usuario + "!");
+        } else {
+            model.addAttribute("mensaje", "Error en la autenticación. Usuario o contraseña incorrectos.");
+        }
+
+        return "index";
+}
+
 }
